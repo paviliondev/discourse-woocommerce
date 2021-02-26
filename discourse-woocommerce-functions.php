c0062dc3bea9e1436556ac96dfbf93b48753736a
angusmcleod <angus@mcleod.org.au>
2021-02-26 16:36:03 +1100

    Update version

--- discourse-woocommerce-functions.php
+++ discourse-woocommerce-functions.php
@@ -4,7 +4,7 @@
  * Plugin Name: Discourse WooCommerce Sync
  * Plugin URI: http://github/paviliondev/discourse-woocommerce-sync
  * Description: Syncs WooCommerce memberships with Discourse groups
- * Version: 0.3.1
+ * Version: 0.3.2
  * Author: Angus McLeod
  * Author URI: http://thepavilion.io
  */
